9729578a0f18d1304c1c8aacccf7f4501e43cb73
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2025-03-08 17:17:49 +0530

    Update environment.prod.ts

--- edubuddy-frontend/src/environments/environment.prod.ts
+++ edubuddy-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
-    production: false,
-    // Firebase configuration is now handled on the backend
-  };
-  
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC6UP9-UAJjrF-J8QorJkBUrZ-BbonlLuw",
+    authDomain: "edubuddy-b63fa.firebaseapp.com",
+    projectId: "edubuddy-b63fa",
+    storageBucket: "edubuddy-b63fa.appspot.com",
+    messagingSenderId: "348763482288",
+    appId: "1:348763482288:web:06aeb5b2807b84ef9d3375"
+  }
+};
